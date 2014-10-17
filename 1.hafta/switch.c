0f9cdd3a86898e6fc2dd8766c947dcff1554ac23
Bilal Tonga <bilaltonga@gmail.com>
2014-10-17 09:54:05 +0300

    Create switch.c

---
+++ 1.hafta/switch.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+	int a;
+	scanf("%d",&a);
+	
+	switch(a){
+		case 1:
+			printf("a birdir");		
+			break;
+		case 2:
+			printf("a ikidir");
+			break;
+		default:
+			printf("a hicbiseydir");
+			break;
+		
+	}
+	
+}
