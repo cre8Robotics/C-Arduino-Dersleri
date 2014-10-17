16c7c7357a3f3e7545517a1aa2c710abcd01077b
Bilal Tonga <bilaltonga@gmail.com>
2014-10-17 09:53:16 +0300

    Create hesap-makinesi.c

---
+++ 1.hafta/hesap-makinesi.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+int main (){
+	
+	float a;
+	float b;
+	float c;
+	int secim;
+	
+	scanf("%d",&a);
+	scanf("%d",&b);
+	
+	printf("ne yapayim sahip ?\n");
+	printf("toplama icin 1\n");
+	printf("cikarma icin 2\n");
+	printf("carpma icin 3\n");
+	printf("bolme icin 4\n");
+	scanf("%d",&secim);
+	
+	switch(secim){
+		case 1:
+			c = a + b;
+			break;
+		case 2:
+			c = a - b;
+			break;
+		case 3:
+			c = a * b;
+			break;
+		case 4:
+			c = a / b;
+			break;
+	}
+	printf("sonuc : %f ",c);
+		
+	getch(); 
+}
