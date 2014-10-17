38f93e14a48296ed6a573beeee01b5acce10b7b8
Bilal Tonga <bilaltonga@gmail.com>
2014-10-17 09:53:43 +0300

    Create if.c

---
+++ 1.hafta/if.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+
+int main(){
+	int a ;
+	scanf("%d",&a);
+	
+	if(a < 5){
+		
+		printf("%d kucuktur besten",a);
+		
+	}else if(a == 5){
+		
+		printf("%d esittir bese",a);
+		
+	}else{
+		
+		printf("%d buyuktur besten",a);
+		
+	}
+	
+}
